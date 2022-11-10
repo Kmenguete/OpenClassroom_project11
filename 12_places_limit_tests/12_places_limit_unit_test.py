85394a1ce51f022bb51a1487ea757fd4d08bd209
Kmenguete <menguetekevin@gmail.com>
2022-11-10 14:08:40 +0100

    testing the creation of a competition and a club object

---
+++ 12_places_limit_tests/12_places_limit_unit_test.py
@@ -0,0 +1,39 @@
+from gudlft.main import create_club, create_competition
+
+
+class ClubMockResponse:
+
+    @staticmethod
+    def get_info():
+        return {"name": "Club Test",
+                "email": "example@gmail.com",
+                "points": "30"}
+
+
+class CompetitionMockResponse:
+
+    @staticmethod
+    def get_info():
+        return {"name": "Competition Test",
+                "date": "2022-06-09 10:00:00",
+                "numberOfPlaces": "50"}
+
+
+def test_create_club(mocker):
+
+    mocker.patch('main.Club', return_value=ClubMockResponse())
+
+    expected_value = {"name": "Club Test",
+                      "email": "example@gmail.com",
+                      "points": "30"}
+    assert create_club() == expected_value
+
+
+def test_create_competition(mocker):
+
+    mocker.patch('main.Competition', return_value=CompetitionMockResponse())
+
+    expected_value = {"name": "Competition Test",
+                      "date": "2022-06-09 10:00:00",
+                      "numberOfPlaces": "50"}
+    assert create_competition() == expected_value
